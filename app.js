4a0b176db084ccf6fea0f8a9790ba1cef5c2c1c1
Nik Arboleda <nik.arboleda@gmail.com>
2019-08-12 18:49:31 -0400

    testing API calls

--- app.js
+++ app.js
@@ -31,13 +31,17 @@ $(document).ready(function () {
           console.log('success');
           console.log(response.response.docs);
 
-          for (i = 0; i<10; i++){
+          for (i = 0; i < 10; i++) {
             console.log('Article #: ' + i)
             console.log(response.response.docs[i].headline.main);
             console.log(response.response.docs[i].snippet);
             console.log(response.response.docs[i].web_url);
-            let image = response.response.docs[i].multimedia[0].url;
-            console.log('https://www.nytimes.com/'+image);
+            if (response.response.docs[i].multimedia[0].url !== undefined) {
+              let image = response.response.docs[i].multimedia[0].url;
+              console.log('https://www.nytimes.com/' + image);
+            } else {
+              console.log('elsed');
+            };
           }
 
         });
@@ -51,47 +55,4 @@ $(document).ready(function () {
 
 
   generateButtons();
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // uSubmit.on('click', function(event) {
-  //   console.log('clicked!');
-  //   let searchTerm = uSearch.val().trim();
-  //   console.log('search term: ' + searchTerm);
-  //
-  //   let searchButton = $("<button>");
-  //   searchButton.addClass('buttonClass searchButton')
-  //   searchButton.text(searchTerm);
-  //   searchButton.data('search', searchTerm);
-  //   searchButton.bind('click', function(){
-  //     console.log($(this.data('search')))
-  //   });
-  //
-  //   if (searchTerm !== '') {
-  //
-  //
-  //     buttonDisplay.append(searchButton);
-  //
-  //   }
-  //
-  //   uSearch.val('');
-  //
-  // });
-  //
-  // $('#clear').on('click', function() {
-  //   $('.buttonClass').remove();
-  // });
-  //
-  //
-  // console.log('ready');
 });
