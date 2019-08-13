820ac4e0bc1efb5d3505a800bec47d68c970b8da
Keith Naber <knaber4124@gmail.com>
2019-08-12 21:32:16 -0400

    buttons appending to DOM dynamically

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
   let uSubmit = $('#userSubmit');
 
   function startOptions() {
-    let trendSpotterQueryURL = 'http://api.trendspottr.com/v1.5/search?key=14a60eb3e939d55be739b830c3419a8b&q=Breaking%20News&w=twitter&resolve_urls=true&n=5'
+    let trendSpotterQueryURL = 'http://api.trendspottr.com/v1.5/search?key=14a60eb3e939d55be739b830c3419a8b&q=Breaking News&w=twitter&resolve_urls=true&n=5'
 
     $.ajax({
       url: trendSpotterQueryURL,
@@ -15,15 +15,16 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log('success');
       console.log(response);
-      let responseArray=response.results.phrases;
-      console.log('success');
-      console.log(response);
-    })
-    for (let i = 0; i < length.responseArray; i++) {
-      topics.push(responseArray[i].value);
-    }
-  }
+      let responseArray = response.results.phrases;
 
+      response.results.phrases.forEach(p => console.log(p));
+      for (let j = 0; j < responseArray.length; j++) {
+        topics.push(responseArray[j].value);
+      }
+      generateButtons();
+    })
+  }
+  console.log(topics);
   uSubmit.on('click', function (event) {
     userGives = uSearch.val().trim();
     if (userGives !== '') {
@@ -35,18 +36,17 @@ $(document).ready(function () {
   const generateButtons = function () {
     buttonDisplay.empty();
     topics.forEach(function (topic) {
-      let button = $('<button>').addClass('buttonClass buttonSearch').on('click', function () {
+      let button = $('<button>').addClass('buttonClass buttonSearch button waves-effect waves-light btn').on('click', function () {
         buttonDisplay.empty();
         let cnnQueryUrl = "https://newsapi.org/v2/everything?sources=cnn&q=" + topic + "&apiKey=d7144e0f89d24c7b9ef1f96d6f4cf7a3";
         let foxQueryURL = 'https://newsapi.org/v2/everything?sources=fox-news&q=' + topic + '&apiKey=d7144e0f89d24c7b9ef1f96d6f4cf7a3';
-        let trendSpotterQueryURL = 'http://api.trendspottr.com/v1.5/search?key=14a60eb3e939d55be739b830c3419a8b& q=breaking news& w=twitter & resolve_urls=true & n=5'
         console.log(queryUrl);
         $.ajax({
           url: cnnQueryUrl,
           method: "GET"
         }).then(function (response) {
           console.log(response);
-          for (i = 0; i < 10; i++) {
+          for (let i = 0; i < 10; i++) {
             console.log(response.articles[i].title);
             console.log(response.articles[i].url);
             console.log(response.articles[i].urlToImage);
@@ -58,10 +58,10 @@ $(document).ready(function () {
         }).then(function (response) {
           console.log('success');
           console.log(response);
-          for (i = 0; i < 10; i++) {
-            console.log(response.articles[i].title);
-            console.log(response.articles[i].url);
-            console.log(response.articles[i].urlToImage);
+          for (let k = 0; k < 10; k++) {
+            console.log(response.articles[k].title);
+            console.log(response.articles[k].url);
+            console.log(response.articles[k].urlToImage);
           }
         })
         generateButtons();
@@ -72,5 +72,4 @@ $(document).ready(function () {
 
 
   startOptions();
-  generateButtons();
 });
