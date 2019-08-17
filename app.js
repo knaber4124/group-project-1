796beb5922ae563c2e85843680375a293ec88e95
Keith Naber <knaber4124@gmail.com>
2019-08-17 13:10:26 -0400

    adding start of if statement to fix no image

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
   genCol.hide();
 
   function startOptions() {
-    let trendSpotterQueryURL = 'http://api.trendspottr.com/v1.5/search?key=e068b56c29a84a5e62c30bdba07cb6f7&q=Breaking News&w=twitter&resolve_urls=true&n=5';
+    let trendSpotterQueryURL = 'http://api.trendspottr.com/v1.5/search?key=2f503c7ab9a2421f954f0c4c06e16f38&q=Breaking News&w=twitter&resolve_urls=true&n=5';
 
     $.ajax({
       url: trendSpotterQueryURL,
@@ -44,9 +44,9 @@ $(document).ready(function () {
         buttonDisplay.empty();
         let foxArticles = null;
         let cnnArticles = null;
-        let cnnQueryUrl = "https://newsapi.org/v2/everything?sources=cnn&q=" + topic + "&apiKey=d7144e0f89d24c7b9ef1f96d6f4cf7a3";
-        let foxQueryUrl = 'https://newsapi.org/v2/everything?sources=fox-news&q=' + topic + '&apiKey=d7144e0f89d24c7b9ef1f96d6f4cf7a3';
-        let generalQueryUrl = 'https://newsapi.org/v2/everything?q=' + topic + '&apiKey=d7144e0f89d24c7b9ef1f96d6f4cf7a3'
+        let cnnQueryUrl = "https://newsapi.org/v2/everything?sources=cnn&q=" + topic + "&apiKey=eb588a8a72464a4286033ee18de5d1fa3";
+        let foxQueryUrl = 'https://newsapi.org/v2/everything?sources=fox-news&q=' + topic + '&apiKey=eb588a8a72464a4286033ee18de5d1fa';
+        let generalQueryUrl = 'https://newsapi.org/v2/everything?q=' + topic + '&apiKey=eb588a8a72464a4286033ee18de5d1fa'
         console.log(generalQueryUrl);
         $.ajax({
           url: cnnQueryUrl,
@@ -56,35 +56,42 @@ $(document).ready(function () {
           console.log(cnnArticles);
           console.log(response);
           console.log(response.articles.length);
-          if (response.articles.length < 1 ){
+          if (response.articles.length < 1) {
             $('.cnnArticle').remove();
             $('#cnnCol').append('<div class="card cnnArticle"><h1>Topic so hot CNN has not reported on it yet</h1></div>');
             $.ajax({
               url: generalQueryUrl,
               method: "GET"
-            }).then(function (response){
+            }).then(function (response) {
               genCol.show();
 
-              for (i=0;i<5;i++){
+              for (i = 0; i < 5; i++) {
+                console.log(response.articles[i].url);
                 let url = response.articles[i].url;
                 console.log(response.articles[i].title);
-                console.log(response.articles[i].url);
-                console.log(response.articles[i].urlToImage);
-                $('#generalCol').append('<div class="card cnnArticle" style="cursor:pointer"><div class="card-panel hoverable">" '+ response.articles[i].title + '</div><div class="card-image"><img src="'+response.articles[i].urlToImage+'"></div></div>').on('click', function(){
+                console.log(response.articles[i].urlToImage)
+                if (response.articles[i].urlToImage === null) {
+                  console.log('No Image') 
+                  let defaultImage = "<img src='https://furcommission.com/wp-content/uploads/New-Images/Home-Page_Slider/News.jpg' alt='Generic News Image'>"
+                  $('.card-image').empty();
+                  $('.card-image').append(defaultImage)
+                }
+                $('#generalCol').append('<div class="card cnnArticle" style="cursor:pointer"><div class="card-panel hoverable">" ' + response.articles[i].title + '</div><div class="card-image"><img src="' + response.articles[i].urlToImage + '"></div></div>').on('click', function () {
                   window.open(url, '_blank');
-                });
+                })
 
-              }
+              };
               console.log(response);
             });
             console.log('no articles really');
           }
 
-          if (response.articles.length > 1 ){
+          if (response.articles.length > 1) {
             $('.cnnArticle').remove();
             for (let i = 0; i < 5; i++) {
-              $('#cnnCol').append('<div class="card cnnArticle" style="cursor:pointer"><div class="card-panel hoverable">" '+ response.articles[i].title + '</div><div class="card-image"><img src="'+response.articles[i].urlToImage+'"></div></div>').on('click', function(){
-                window.open(response.articles[i].url, '_blank');
+              let url = response.articles[i].url;
+              $('#cnnCol').append('<div class="card cnnArticle" style="cursor:pointer"><div class="card-panel hoverable">" ' + response.articles[i].title + '</div><div class="card-image"><img src="' + response.articles[i].urlToImage + '"></div></div>').on('click', function () {
+                window.open(url, '_blank');
               });
             }
           }
@@ -95,17 +102,18 @@ $(document).ready(function () {
           }).then(function (response) {
             foxArticles = response.articles;
             console.log(response);
-            if (response.articles.length < 1 ){
+            if (response.articles.length < 1) {
               genCol.show();
               $('.foxArticle').remove();
               $('#foxCol').append('<div class="card foxArticle"><h1>Topic so hot FOX has not reported on it yet</h1></div>');
               console.log('no articles really');
             }
-            if (response.articles.length > 1 ){
+            if (response.articles.length > 1) {
               $('.foxArticle').remove();
               for (let i = 0; i < 5; i++) {
-                $('#foxCol').append('<div class="card foxArticle" style="cursor:pointer"><div class="card-panel hoverable">" '+ response.articles[i].title + '</div><div class="card-image"><img src="'+response.articles[i].urlToImage+'"></div></div>').on('click', function(){
-                  window.open(response.articles[i].url, '_blank');
+                let url = response.articles[i].url;
+                $('#foxCol').append('<div class="card foxArticle" style="cursor:pointer"><div class="card-panel hoverable">" ' + response.articles[i].title + '</div><div class="card-image"><img src="' + response.articles[i].urlToImage + '"></div></div>').on('click', function () {
+                  window.open(url, '_blank');
                 });
               }
             }
