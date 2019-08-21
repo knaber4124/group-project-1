aece035d5120b7d6ad210a0c861068dd156814d6
Keith Naber <knaber4124@gmail.com>
2019-08-21 18:33:35 -0400

    removed console.log

--- app.js
+++ app.js
@@ -12,13 +12,11 @@ $(document).ready(function () {
   function startOptions() {
     let trendSpotterQueryURL = 'https://api.trendspottr.com/v1.5/search?key=0e6b120786bf3ca210a83184e06ca250&q=Breaking News&w=twitter&resolve_urls=true&n=5';
 
-    
     $.ajax({
       url: trendSpotterQueryURL,
       method: 'GET'
     }).then(function (response) {
-      console.log('success');
-      console.log(response);
+      
       let responseArray = response.results.phrases;
 
       response.results.phrases.forEach(p => console.log(p));
@@ -28,7 +26,7 @@ $(document).ready(function () {
       generateButtons();
     });
   }
-  console.log(topics);
+  
   uSubmit.on('click', function (event) {
     userGives = uSearch.val().trim();
     if (userGives !== '' && !topics.includes(userGives)) {
@@ -38,7 +36,7 @@ $(document).ready(function () {
     }
   });
   $(document).keypress(function (event) {
-    console.log('key pressed');
+    
     userGives = uSearch.val().trim();
     if (event.which == 13 && userGives !== '' && !topics.includes(userGives)) {
       topics.push(userGives);
@@ -57,15 +55,13 @@ $(document).ready(function () {
         let cnnQueryUrl = "https://newsapi.org/v2/everything?sources=cnn&q=" + topic + "&apiKey=d7144e0f89d24c7b9ef1f96d6f4cf7a3&pageSize=5";
         let foxQueryUrl = 'https://newsapi.org/v2/everything?sources=fox-news&q=' + topic + '&apiKey=d7144e0f89d24c7b9ef1f96d6f4cf7a3&pageSize=5';
         let generalQueryUrl = 'https://newsapi.org/v2/everything?q=' + topic + '&apiKey=d7144e0f89d24c7b9ef1f96d6f4cf7a3&pageSize=5'
-        console.log(generalQueryUrl);
+        
         $.ajax({
           url: cnnQueryUrl,
           method: "GET"
         }).then(function (response) {
           cnnArticles = response.articles;
-          console.log(cnnArticles);
-          console.log(response);
-          console.log(response.articles.length);
+          
           if (response.articles.length < 1) {
             $('.cnnArticle').remove();
             $('#cnnCol').append('<div class="card cnnArticle"><h1>Topic so hot CNN has not reported on it yet</h1></div>');
@@ -76,12 +72,9 @@ $(document).ready(function () {
               genCol.show();
 
               for (i = 0; i < 5; i++) {
-                console.log(response.articles[i].url);
+               
                 let url = response.articles[i].url;
-                console.log(url);
-                console.log(response.articles[i].title);
-                console.log(response.articles[i].url);
-                console.log(response.articles[i].urlToImage);
+                
                 $('#generalCol').append('<a target="_blank" href="'+response.articles[i].url+'"><div class="card cnnArticle" style="cursor:pointer"><div class="card-panel hoverable">" ' + response.articles[i].title + '</div><div class="card-image"><img src="' + response.articles[i].urlToImage + '"></div></div></a>')
 
               };
@@ -103,7 +96,7 @@ $(document).ready(function () {
             for (let i = 0; i < 5; i++) {
               // createCards("#cnnCol", response.articles[i].url, response.articles[i].title)
               let url = response.articles[i].url;
-              console.log(url);
+              
               $('#cnnCol').append('<a target="_blank" href=' + url + '><div class="card cnnArticle" style="cursor:pointer"><div class="card-panel hoverable"> ' + response.articles[i].title + '</div><div class="card-image"><img src="' + response.articles[i].urlToImage + '"></div></div></a>');
             }
           }
@@ -113,12 +106,12 @@ $(document).ready(function () {
             method: 'GET'
           }).then(function (response) {
             foxArticles = response.articles;
-            console.log(response);
+            
             if (response.articles.length < 1) {
               genCol.show();
               $('.foxArticle').remove();
               $('#foxCol').append('<div class="card foxArticle"><h1>Topic so hot FOX has not reported on it yet</h1></div>');
-              console.log('no articles really');
+              
             }
             if (response.articles.length > 1) {
               $('.foxArticle').remove();
